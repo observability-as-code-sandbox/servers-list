c18677cacebee9de0c835abb2ec338ea2ce99597
alex_jov <aleksjovovic@gmail.com>
2022-01-17 14:56:52 +0000

    Add script to convert json to csv;

---
+++ convert-servers-list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dt=$(date '+%Y-%m-%d_%H-%M-%S')
+
+source_dir="./exported/"
+source_file="servers.json"
+
+destination_dir="./exported/"
+
+$(cat ${source_dir}${source_file} | jq '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' > ${destination_dir}servers-${dt}.csv)
