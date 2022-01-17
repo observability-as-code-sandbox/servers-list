0bc30615aa27a60fd5562a831cdb3a41bd4d3304
alex_jov <aleksjovovic@gmail.com>
2022-01-17 17:28:18 +0000

    Add space to sed command;

--- convert-servers-list.sh
+++ convert-servers-list.sh
@@ -7,4 +7,4 @@ source_file="servers.json"
 
 destination_dir="./exported/"
 
-$(cat ${source_dir}${source_file} | jq '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' | sed 's/\\"//g' > ${destination_dir}servers-${dt}.csv)
+$(cat ${source_dir}${source_file} | jq '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' | sed 's/\\"//g'> ${destination_dir}servers-${dt}.csv)
